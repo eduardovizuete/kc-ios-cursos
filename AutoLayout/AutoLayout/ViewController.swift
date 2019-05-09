049f5dfb7b815e4d081eaba83d9a12cac82ae6a9
Eduardo Vizuete <gilberto.vizuete@modinter.com.ec>
2019-05-09 14:38:58 -0500

    AutoLayout: initial project creation

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by eduardo on 5/9/19.
+//  Copyright © 2019 eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
